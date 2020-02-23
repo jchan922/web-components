d99959c710af3ef8cbf2e6f74629eefdf9eb137f
jchan <jchan@fandango.com>
2020-02-22 16:52:36 -0800

    finishing assignment 1

--- toggle-button.js
+++ toggle-button.js
@@ -2,35 +2,29 @@ class ToggleButton extends HTMLElement {
     constructor() {
         super();
         this.attachShadow({ mode: 'open' });
-        this.textContent = 'Default text content';
         this.shadowRoot.innerHTML = `
-            <style>
-                #info-box.hidden { display: none; }
-            </style>
-            <button type="button">Show</button>
-            <p id="info-box" class="hidden">
-                <slot></slot>
-            </p>
+        <style>
+        #info-box.hidden { display: none; }
+        </style>
+        <button type="button">Show</button>
+        <p id="info-box" class="hidden">
+        <slot></slot>
+        </p>
         `;
         this._button = this.shadowRoot.querySelector('button');
         this._infoBox = this.shadowRoot.getElementById('info-box');
-
         this._button.addEventListener('click', this._toggleInfoBox.bind(this));
     }
 
     connectedCallback() {
-        // if (this.getAttribute('is-visible') === 'true') {
-        //     debugger
-        //     this._toggleInfoBox();
-        // }
-
+        this.getAttribute('is-visible') === 'true' && this._toggleInfoBox();
     }
 
     _toggleInfoBox() {
-        // this._infoBox.classList.contains('hidden')
-        //     ? this._button.textContent = 'Hide'
-        //     : this._button.textContent = 'Show'; 
-        // this._infoBox.classList.toggle('hidden');
+        this._infoBox.classList.contains('hidden')
+            ? this._button.textContent = 'Hide'
+            : this._button.textContent = 'Show'; 
+        this._infoBox.classList.toggle('hidden');
     }
 }
 
